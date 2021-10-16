0839da7f7bb930370506ec878f4569f37c499b33
Drishti Rana <81031606+dishi24@users.noreply.github.com>
2021-10-16 23:33:21 +0530

    Update Treasure Island.py

--- Treasure Island.py
+++ Treasure Island.py
@@ -23,7 +23,6 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.") 
 
-#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
 print("You are standing on a crossroad")
 turn = input("Where you want to turn? right or left?  ")
